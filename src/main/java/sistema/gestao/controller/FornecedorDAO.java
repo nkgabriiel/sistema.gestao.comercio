a0bbe74f06d846a1066fcd7f79c52b232f870d4c
nkgabriiel <gabriel.3930@gmail.com>
2026-04-28 17:40:04 -0300

    Finalização FornecedorDAO.

--- src/main/java/sistema/gestao/controller/FornecedorDAO.java
+++ src/main/java/sistema/gestao/controller/FornecedorDAO.java
@@ -3,6 +3,8 @@ package sistema.gestao.controller;
 import sistema.gestao.model.Fornecedor;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FornecedorDAO {
     public void criarFornecedor(Fornecedor fornecedor) {
@@ -57,4 +59,79 @@ public class FornecedorDAO {
         }
         return fornecedorEncontrado;
     }
+
+    public List<Fornecedor> listarFornecedores() {
+        List<Fornecedor> listaFornecedores = new ArrayList<>();
+
+        String sql = """
+                SELECT * FROM fornecedor;
+                """;
+
+        try (Connection conn = ConnectionFactory.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery()) {
+
+            while(rs.next()) {
+                Fornecedor fornecedor = new Fornecedor(
+                        rs.getString("cnpj"),
+                        rs.getString("nome_fantasia"),
+                        rs.getString("nome_representante"),
+                        rs.getString("contato_representante")
+                );
+                fornecedor.setId(rs.getLong("id"));
+                listaFornecedores.add(fornecedor);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao listar fornecedores." + e.getMessage());
+        }
+        return listaFornecedores;
+    }
+
+    public boolean atualizarFornecedor(Fornecedor fornecedor) {
+        String sql = """
+                UPDATE fornecedor SET nome_representante = ?, contato_representante = ?, nome_fantasia = ?, cnpj = ?
+                WHERE id = ?;
+                """;
+
+        boolean fornecedorAtualizado = false;
+
+        try (Connection conn = ConnectionFactory.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, fornecedor.getNomeRepresentante());
+            stmt.setString(2, fornecedor.getContatoRepresentante());
+            stmt.setString(3, fornecedor.getNomeFantasia());
+            stmt.setString(4, fornecedor.getCnpj());
+            stmt.setLong(5, fornecedor.getId());
+
+            int linhasAfetadas = stmt.executeUpdate();
+
+            if(linhasAfetadas > 0) {
+                fornecedorAtualizado = true;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar fornecedor." + e.getMessage());
+        }
+        return fornecedorAtualizado;
+    }
+
+    public void deletarFornecedor (Long id) {
+        String sql = """
+                DELETE FROM fornecedor WHERE id = ?;
+                """;
+
+        try (Connection conn = ConnectionFactory.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+            System.out.println("Fornecedor deletado com sucesso.");
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao deletar fornecedor." + e.getMessage());
+        }
+
+    }
+
 }
